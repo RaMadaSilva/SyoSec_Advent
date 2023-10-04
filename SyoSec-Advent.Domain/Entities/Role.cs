a2f33283ab7090c2921a4013570eaa7db8c25c5b
RaMadaSilva <raul01mateia@gmail.com>
2023-10-04 09:23:35 +0100

    Criando a entidade user e role

---
+++ SyoSec-Advent.Domain/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace SyoSec_Advent.Domain.Entities
+{
+    public class Role : Entity
+    {
+        public string RoleName { get; private set; }
+        public IReadOnlyCollection<User> Users { get; private set; }
+    }
+}
---
+++ SyoSec-Advent.Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using SyoSec_Advent.Domain.ValueObject;
+
+namespace SyoSec_Advent.Domain.Entities
+{
+    public class User : Entity
+    {
+        private List<Role> _roles = new();
+        private User()
+        {
+            
+        }
+        public User(Name fullName, string userName, string passwordHash)
+        {
+            FullName = fullName;
+            UserName = userName;
+            PasswordHash = passwordHash;
+        }
+
+        public Name FullName { get; private set; }
+        public string UserName { get; private set; }
+        public string PasswordHash { get; private set; }
+        public IReadOnlyCollection<Role> Roles { get => _roles;}
+
+        public void AddRole(Role role) => _roles.Add(role); 
+
+    }
+}
