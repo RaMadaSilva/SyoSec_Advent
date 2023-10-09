fbd22daae46400f41e93f97d92be517d6aecd7cc
RaMadaSilva <raul01mateia@gmail.com>
2023-10-09 21:03:35 +0100

    Recommendation Red

---
+++ SyoSec-Advent.Test/Test.Domain.Entity/RecommendationTest.cs
@@ -0,0 +1,29 @@
+﻿namespace SyoSec_Advent.Test.Test.Domain.Entity
+{
+    [TestClass]
+    public class RecommendationTest
+    {
+        [TestMethod]
+        public void DadaUmaRecomendacaoDentroDoPrazoActualizarOEstadoParaInvalido()
+        {
+            Assert.Fail(); 
+        }
+
+        [TestMethod]
+        public void DadaUmaRecomendacaoForaDoPrazoActualizarOEstadoParaDevololvido()
+        {
+            Assert.Fail(); 
+        }
+        public void DadaUmaRecomendacaoActualizarOEstadoParaDevololvido()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void DadaUmaRecomendacaoActualizarOEstadoParaTransferencia()
+        {
+            Assert.Fail(); 
+        }
+        
+    }
+}
