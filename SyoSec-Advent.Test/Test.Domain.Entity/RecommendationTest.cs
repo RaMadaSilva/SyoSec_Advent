8ae28b0faa2c3418f5f263950f1cd1a8f5b6f0fc
RaMadaSilva <raul01mateia@gmail.com>
2023-10-09 21:37:22 +0100

    Green Refactory

--- SyoSec-Advent.Test/Test.Domain.Entity/RecommendationTest.cs
+++ SyoSec-Advent.Test/Test.Domain.Entity/RecommendationTest.cs
@@ -1,4 +1,8 @@
-﻿namespace SyoSec_Advent.Test.Test.Domain.Entity
+﻿using SyoSec_Advent.Domain.Entities;
+using SyoSec_Advent.Domain.Enums;
+using SyoSec_Advent.Domain.ValueObject;
+
+namespace SyoSec_Advent.Test.Test.Domain.Entity
 {
     [TestClass]
     public class RecommendationTest
@@ -6,7 +10,14 @@
         [TestMethod]
         public void DadaUmaRecomendacaoDentroDoPrazoActualizarOEstadoParaInvalido()
         {
-            Assert.Fail(); 
+            var adress = new Address("cazenga", "Luanda", "Luanda");
+            var adressChurch = new Address("Viana", "Zango", "Luanda"); 
+            var name = new Name("Raul", "Silva"); 
+            var member = new Member(name, "923568923",adress );
+            var church = new Church("Mabor", adressChurch); 
+            var recomendacao = new Recommendation(member,church, "O referido está sozinho", ERecommendationType.Recebida);
+            var result = recomendacao.UpdateStateRecommendationToInvalide(); 
+            Assert.AreEqual(false, result); 
         }
 
         [TestMethod]
