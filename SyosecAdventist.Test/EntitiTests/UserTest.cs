78cd767dd10d61830809c5f3e1cf5d60cd673077
RaMadaSilva <raul01mateia@gmail.com>
2023-10-30 14:24:28 +0100

    test Users

---
+++ SyosecAdventist.Test/EntitiTests/UserTest.cs
@@ -0,0 +1,36 @@
+﻿namespace SyosecAdventist.Test.EntitiTests
+{
+    public class UserTest
+    {
+        [Fact]
+        public void WhenValidUser_ResultSucess()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+        public void WhenValidUser_UpdateUser()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+        [Fact]
+        public void WhenValidUser_UpdatePassword()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail(string.Empty);
+
+        }
+    }
+}
