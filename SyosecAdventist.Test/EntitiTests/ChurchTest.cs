9809f28ae465d7f92fd0e2161e19c262c7e0e709
RaMadaSilva <raul01mateia@gmail.com>
2023-10-30 14:19:12 +0100

    construindo para as entidades

---
+++ SyosecAdventist.Test/EntitiTests/ChurchTest.cs
@@ -0,0 +1,47 @@
+﻿namespace SyosecAdventist.Test.EntitiTests
+{
+    public class ChurchTest
+    {
+        [Fact]
+        public void WhenValidChurch_ResultSucess()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty); 
+        }
+        public void WhenInvalidChurch_ResultError()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+        [Fact]
+        public void WhenChurchExists_NoSaveReturnError()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail(string.Empty); 
+
+        }
+
+        [Fact]
+        public void WhenChurchExists_AddedRecommendations()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail(string.Empty); 
+        }
+    }
+}
---
+++ SyosecAdventist.Test/EntitiTests/MemberTests.cs
@@ -0,0 +1,47 @@
+﻿namespace SyosecAdventist.Test.EntitiTests
+{
+    public  class MemberTests
+    {
+        [Fact]
+        public void WhenValidMember_ResultSucess()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+        public void WhenInvalidMember_ResultError()
+        {
+            //Arrage
+
+            //act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+        [Fact]
+        public void WhenMemberExists_NoSaveReturnError()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail(string.Empty);
+
+        }
+
+        [Fact]
+        public void WhenMemberExists_AddedRecommendations()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail(string.Empty);
+        }
+    }
+}
