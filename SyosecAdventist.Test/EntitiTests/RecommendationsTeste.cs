1e2a5c97362f4dd5ae4850052dbd7679f932895c
RaMadaSilva <raul01mateia@gmail.com>
2023-11-01 19:51:31 +0100

    iniciando a criação dos testes para as recommendations

---
+++ SyosecAdventist.Test/EntitiTests/RecommendationsTeste.cs
@@ -0,0 +1,40 @@
+﻿namespace SyosecAdventist.Test.EntitiTests
+{
+    public class RecommendationsTeste
+    {
+        [Fact]
+        public void WhenReceommendationsReceivedValid_ReturnSucess()
+        {
+
+        }
+        [Fact]
+        public void WhenRecommendationsIssuedValid_ReturnSucess()
+        {
+
+        }
+
+        [Fact]
+        public void WhenRecommendationsReceivedValid_UpdateStateToInvalid()
+        {
+
+        }
+
+        [Fact]
+        public void WhenRecommendationsIssuedValid_UpdateStateToInvalid()
+        {
+
+        }
+
+        [Fact]
+        public void WhenRecommendationsReceivedValid_UpdateStateToDevolvido()
+        {
+
+        }
+
+        [Fact]
+        public void WhenRecommendationsIssuedValid_UpdateStateToDevolvido()
+        {
+
+        }
+    }
+}
