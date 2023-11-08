509fbdba2a386d9bd304c1d21432376919bc8b50
RaMadaSilva <raul01mateia@gmail.com>
2023-11-08 17:31:35 +0100

    fazendo os testes falhar

--- SyosecAdventist.Test/EntitiTests/RecommendationsTeste.cs
+++ SyosecAdventist.Test/EntitiTests/RecommendationsTeste.cs
@@ -5,36 +5,67 @@
         [Fact]
         public void WhenReceommendationsReceivedValid_ReturnSucess()
         {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada"); 
 
         }
         [Fact]
         public void WhenRecommendationsIssuedValid_ReturnSucess()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada");
         }
 
         [Fact]
         public void WhenRecommendationsReceivedValid_UpdateStateToInvalid()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada");
         }
 
         [Fact]
         public void WhenRecommendationsIssuedValid_UpdateStateToInvalid()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada");
         }
 
         [Fact]
         public void WhenRecommendationsReceivedValid_UpdateStateToDevolvido()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada");
         }
 
         [Fact]
         public void WhenRecommendationsIssuedValid_UpdateStateToDevolvido()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
+            Assert.Fail("falha propositada");
         }
     }
 }
