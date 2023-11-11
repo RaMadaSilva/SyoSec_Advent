5c57f539fa9dc1e88bdaf77ba11931a70c7de533
RaMadaSilva <raul01mateia@gmail.com>
2023-11-12 00:46:14 +0100

    Aprontando o Response do getById para das recommendations

--- SyosecAdvent.Application/Recommendations/GetById/GetByIdResponse.cs
+++ SyosecAdvent.Application/Recommendations/GetById/GetByIdResponse.cs
@@ -1,6 +1,27 @@
-﻿namespace SyosecAdvent.Application.Recommendations.GetById
+﻿using SyosecAdvent.Application.Common;
+using SyosecAdvent.Application.Common.DTO;
+
+namespace SyosecAdvent.Application.Recommendations.GetById
 {
-    public class GetByIdResponse
+    public class GetByIdResponse: Response
     {
+        public GetByIdResponse()
+        {
+            
+        }
+        public GetByIdResponse(string message, int status)
+        {
+            Message = message; 
+            StatusCode = status; 
+        }
+
+        public GetByIdResponse(string message, RecommendationDto data)
+        {
+            Message = message; 
+            Data = data;
+            StatusCode = 200; 
+        }
+
+        public RecommendationDto Data { get; set; }
     }
 }
