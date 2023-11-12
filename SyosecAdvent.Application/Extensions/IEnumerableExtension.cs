d9fe326e500e909e6b57642154061aedb06cd2d2
RaMadaSilva <raul01mateia@gmail.com>
2023-11-12 12:33:40 +0100

    refatornado

--- SyosecAdvent.Application/Extensions/IEnumerableExtension.cs
+++ SyosecAdvent.Application/Extensions/IEnumerableExtension.cs
@@ -4,14 +4,14 @@ namespace SyosecAdvent.Application.Extensions
 {
     public static class IEnumerableExtension
     {
-        public static PaginatedListResponse<T> ToPaginationsList<T>(this IEnumerable<T> source, 
-            int currentPage, 
+        public static PaginatedListResponse<T> ToPaginationsList<T>(this IEnumerable<T> source,
+            int currentPage,
             int pageSize) where T : class
         {
-            int  totalItems = source.Count();
-            var Items = source.Skip((currentPage -1)*pageSize).Take(pageSize).ToList();
+            int totalItems = source.Count();
+            var Items = source.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
 
-            return new PaginatedListResponse<T>(Items, totalItems, currentPage, pageSize); 
+            return new PaginatedListResponse<T>(Items, totalItems, currentPage, pageSize);
         }
     }
 }
