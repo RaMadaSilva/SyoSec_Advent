25e3eaf1198c2f9a3245560429eb9f0c03ab51b7
RaMadaSilva <raul01mateia@gmail.com>
2023-11-13 15:05:30 +0100

    implementado a actualização automatica do update state to Invalid

---
+++ SyosecAdvent.Application/UseCase/Recommendations/Update/UpdateRecommentaiosToInvalidAutomatic.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using SyosecAdvent.Domain.Entities;
+
+namespace SyosecAdvent.Application.UseCase.Recommendations.Update
+{
+    public class UpdateRecommentaiosToInvalidAutomatic : IHostedService, IDisposable
+    {
+        private readonly Recommendation _recommendation; 
+        private Timer _timer;
+
+        public UpdateRecommentaiosToInvalidAutomatic(Recommendation recommendation)
+        {
+            _recommendation = recommendation;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(UpdateToInvalid, null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        private void UpdateToInvalid(object state)
+        {
+            _recommendation.UpdateStateRecommendationToInvalide(); 
+        }
+    }
+}
