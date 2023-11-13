76bbdfc5aa427fd9b83bfce16bf6a9ff3acd7dd7
RaMadaSilva <raul01mateia@gmail.com>
2023-11-13 14:43:44 +0100

    Finalizando a validação do Update Retorn Request

--- SyosecAdvent.Application/UseCase/Recommendations/Update/ValidationUpdateRetornRequest.cs
+++ SyosecAdvent.Application/UseCase/Recommendations/Update/ValidationUpdateRetornRequest.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
 
 namespace SyosecAdvent.Application.UseCase.Recommendations.Update
 {
-    internal class ValidationUpdateRetornRequest
+    public static class ValidationUpdateRetornRequest
     {
+        public static Contract<Notification> Ensure(UpdateRetornRequest request)
+            => new Contract<Notification>()
+                    .Requires()
+                    .IsGreaterThan(request.UrlRecommendations, 20, "UrlRecommendations", "A URL deve ter mais de 20 caracteres"); 
     }
 }
