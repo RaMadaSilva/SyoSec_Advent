b31f04de24ecc38027c689f731c7c4b4bfbc67d2
RaMadaSilva <raul01mateia@gmail.com>
2023-11-14 15:03:11 +0100

    Mapeando a entidade membro

--- SyosecAdvent.Infrastructure/Data/Map/MemberMap.cs
+++ SyosecAdvent.Infrastructure/Data/Map/MemberMap.cs
@@ -1,6 +1,60 @@
-﻿namespace SyosecAdvent.Infrastructure.Data.Map
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyosecAdvent.Domain.Entities;
+
+namespace SyosecAdvent.Infrastructure.Data.Map
 {
-    public class MemberMap
+    public class MemberMap : IEntityTypeConfiguration<Member>
     {
+        public void Configure(EntityTypeBuilder<Member> builder)
+        {
+            builder.ToTable(nameof(Member));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=>x.Id)
+                   .IsRequired()
+                   .HasColumnName("Id")
+                   .HasColumnType("BIGINT")
+                   .ValueGeneratedOnAdd();
+
+            builder.OwnsOne(x => x.NameMember)
+                   .Property(x => x.FirstName)
+                   .HasColumnName("Name")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(100);             
+            
+            builder.OwnsOne(x => x.NameMember)
+                   .Property(x => x.LastName)
+                   .HasColumnName("Sobrenome")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(100);
+
+            builder.Property(x => x.Phone)
+                   .HasColumnName("Phone")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(20);
+
+            builder.OwnsOne(x => x.AddressMember)
+                   .Property(x => x.Street)
+                   .HasColumnName("Street")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(100); 
+
+            builder.OwnsOne(x => x.AddressMember)
+                   .Property(x => x.City)
+                   .HasColumnName("City")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(100); 
+
+            builder.OwnsOne(x => x.AddressMember)
+                   .Property(x => x.Province)
+                   .HasColumnName("Province")
+                   .HasColumnType("NVARCHAR")
+                   .HasMaxLength(100); 
+
+            //TODO: Relacionamento
+
+        }
     }
 }
