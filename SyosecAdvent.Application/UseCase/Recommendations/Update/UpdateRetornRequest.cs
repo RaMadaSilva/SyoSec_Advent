126baee6913803eb1625cb48758abc04fa368430
RaMadaSilva <raul01mateia@gmail.com>
2023-11-14 14:49:12 +0100

    mapeamento da Igreja

--- SyosecAdvent.Application/UseCase/Recommendations/Update/UpdateRetornRequest.cs
+++ SyosecAdvent.Application/UseCase/Recommendations/Update/UpdateRetornRequest.cs
@@ -1,9 +1,8 @@
 ﻿using MediatR;
-using SyosecAdvent.Application.Common;
 
 namespace SyosecAdvent.Application.UseCase.Recommendations.Update
 {
-    public  class UpdateRetornRequest : IRequest<UpdateResponse>
+    public class UpdateRetornRequest : IRequest<UpdateResponse>
     {
         public long Id { get; set; }
         public string? UrlRecommendations { get; set; }
--- SyosecAdvent.Infrastructure/Data/Map/ChurchMap.cs
+++ SyosecAdvent.Infrastructure/Data/Map/ChurchMap.cs
@@ -1,6 +1,51 @@
-﻿namespace SyosecAdvent.Infrastructure.Data.Map
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyosecAdvent.Domain.Entities;
+
+namespace SyosecAdvent.Infrastructure.Data.Map
 {
-    public class ChurchMap
+    public class ChurchMap : IEntityTypeConfiguration<Church>
     {
+        public void Configure(EntityTypeBuilder<Church> builder)
+        {
+            builder.ToTable(nameof(Church));
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                   .IsRequired()
+                   .HasColumnName("Id")
+                   .HasColumnType("BIGINT")
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.NameChurch)
+                    .IsRequired()
+                    .HasColumnName("Name")
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(100);
+
+            builder.OwnsOne(x=>x.AddressChurch)
+                    .Property(x=>x.Street)
+                    .IsRequired()
+                    .HasColumnName("Rua")
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(200);            
+            
+            builder.OwnsOne(x=>x.AddressChurch)
+                    .Property(x=>x.City)
+                    .IsRequired()
+                    .HasColumnName("Cidade")
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(200);            
+            
+            builder.OwnsOne(x=>x.AddressChurch)
+                    .Property(x=>x.Province)
+                    .IsRequired()
+                    .HasColumnName("Provincia")
+                    .HasColumnType("NVARCHAR")
+                    .HasMaxLength(200);
+
+            //TODO: Relacionamento
+
+        }
     }
 }
