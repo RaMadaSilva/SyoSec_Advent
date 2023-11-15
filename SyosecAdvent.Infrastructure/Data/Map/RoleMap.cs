05e353865baa41c16579be474dafb4d37b6aba6a
RaMadaSilva <raul01mateia@gmail.com>
2023-11-15 12:23:50 +0100

    map  da entidade role

--- SyosecAdvent.Infrastructure/Data/Map/RoleMap.cs
+++ SyosecAdvent.Infrastructure/Data/Map/RoleMap.cs
@@ -1,6 +1,30 @@
-﻿namespace SyosecAdvent.Infrastructure.Data.Map
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyosecAdvent.Domain.Entities;
+
+namespace SyosecAdvent.Infrastructure.Data.Map
 {
-    public class RoleMap
+    public class RoleMap : IEntityTypeConfiguration<Role>
     {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable(nameof(Role));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x=>x.Id)
+                .IsRequired()
+                .HasColumnName("Id")
+                .HasColumnType("BIGINT")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.RoleName)
+                .IsRequired()
+                .HasColumnName("RoleName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50); 
+
+            //TODO: Relacionamento
+        }
     }
 }
