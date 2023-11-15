375de9b403f516c29e7d4297099eb55098b166de
RaMadaSilva <raul01mateia@gmail.com>
2023-11-15 13:18:14 +0100

    mapeando a entidade user

--- SyosecAdvent.Infrastructure/Data/Map/UserMap.cs
+++ SyosecAdvent.Infrastructure/Data/Map/UserMap.cs
@@ -1,6 +1,50 @@
-﻿namespace SyosecAdvent.Infrastructure.Data.Map
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyosecAdvent.Domain.Entities;
+
+namespace SyosecAdvent.Infrastructure.Data.Map
 {
-    public class UserMap
+    public class UserMap : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .IsRequired()
+                .HasColumnName("Id")
+                .HasColumnType("BIGINT")
+                .ValueGeneratedOnAdd();
+
+            builder.OwnsOne(x => x.FullName)
+                .Property(x => x.FirstName)
+                .IsRequired()
+                .HasColumnName("FistName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.OwnsOne(x => x.FullName)
+                .Property(x => x.LastName)
+                .IsRequired()
+                .HasColumnName("LastName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.UserName)
+                .IsRequired()
+                .HasColumnName("UserName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired()
+                .HasColumnName("PasswordHash")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            //TODO: Relacionamento
+        }
     }
 }
