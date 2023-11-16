c28d71f0a15df74f497e5b5d6bd1d2ca322bd781
RaMadaSilva <raul01mateia@gmail.com>
2023-11-16 12:52:09 +0100

    iniciando a implementação dos repositorio base de leitura e de escrita

---
+++ SyosecAdvent.Infrastructure/Repositories/BaseReadRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using SyosecAdvent.Domain.Entities;
+using SyosecAdvent.Domain.Interfaces.Repositories;
+
+namespace SyosecAdvent.Infrastructure.Repositories
+{
+    public class BaseReadRepositoryAsync<TEntity> : IBaseReadRepositoryAsync<TEntity> where TEntity : Entity
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public  async Task<TEntity> GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SyosecAdvent.Infrastructure/Repositories/BaseWriteRepositoryAsync.cs
@@ -0,0 +1,23 @@
+﻿using SyosecAdvent.Domain.Entities;
+using SyosecAdvent.Domain.Interfaces.Repositories;
+
+namespace SyosecAdvent.Infrastructure.Repositories
+{
+    public class BaseWriteRepositoryAsync<TEntity> : IBaseWriteRepositoryAsync<TEntity> where TEntity : Entity
+    {
+        public async Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
